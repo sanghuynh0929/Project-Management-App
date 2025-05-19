9520e0819b455f2bfedf819641be107e07ceac63
huumai1903 <146205608+huumai1903@users.noreply.github.com>
2025-05-20 00:26:23 +0700

    Epic REST API implementation

--- src/main/java/com/thesis/projectmanagement/controllers/ProjectManagementController.java
+++ src/main/java/com/thesis/projectmanagement/controllers/ProjectManagementController.java
@@ -1,4 +1,4 @@
-package com.thesis.projectmanagement;
+package com.thesis.projectmanagement.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
